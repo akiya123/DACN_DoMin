be2a82950b6ac84a3fba6307ea18e83f932ded91
Nguyễn Như Hoàn <92707784+akiya123@users.noreply.github.com>
2026-09-24 19:19:49 +0700

    Tuần 2 báo cáo

---
+++ unity-mine-sweeper-master/Assets/Scripts/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tag
+{
+    public class GameBox
+    {
+        public const string box = "box";
+        public const string boxProduction = "boxProduction";
+        public const string sixteenBoxProduction = "sixteenProduction";
+        public const string sixteenbox = "sixteenbox";
+        public const string thirtybox = "thirtybox";
+        public const string thirtyBoxProduction = "thirtyProduction";
+    }
+
+    public class UI
+    {
+        public const string Condition = "Condition";
+        public const string sixteenCondition = "sixteenCondition";
+        public const string thirtyCondition = "thirtyCondition";
+    }
+}
---
+++ unity-mine-sweeper-master/README.md
@@ -0,0 +1,21 @@
+# MineSweeper/踩地雷
+
+## English
+
+MineSweeper is Made with Unity.
+
+This MineSweeper has three mode to play.
+
+Three Mode:
+10x10
+16x16
+30x30
+
+## 中文
+
+使用Unity還原經典遊戲踩地雷。
+
+有三個模式供玩家選擇，分別是10x10,16x16,30x30。
+
+Example: 30x30
+<img src="https://raw.githubusercontent.com/tsen1220/UnityMineSweeper/master/IMG/introduction.jpg">
